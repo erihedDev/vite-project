c44d547f6d610c2a39aae3e10bfc8dfc846057e8
erihedDev <erik.hedlund@elev.ga.ntig.se>
2022-10-07 11:55:32 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://<erihedDev>.github.io/<portfolio>/',
+  base: '/project/',
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600,
-  },
+  
 })
